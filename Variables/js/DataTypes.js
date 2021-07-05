a92c968728c9bc8cb419ce1ffbce5c7498a39e9b
lucaspalleros <lucaspalleros@gmail.com>
2021-07-05 00:11:14 -0300

    folders

---
+++ Variables/js/DataTypes.js
@@ -0,0 +1,13 @@
+//Variables and DataTypes
+
+// Number Type Values 
+let number = 12; 
+console.log( number );
+
+// String Value
+let name = "John";
+
+// Boolean value 
+let boolean = true; // true or false
+
+console.log( boolean );
---
+++ Variables/js/Variables.js
@@ -0,0 +1,9 @@
+console.log("# Variables");
+
+//Variables
+let name = "John"
+
+
+console.log("Hello " + name);
+
+
